42f247b583dfeef7a6db00eeaf8ca3de0a0db969
Rishalg <rishalgedam@gmail.com>
2024-02-22 07:18:45 -0600

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MotherNature from "./components/MotherNature";
 import Layout from "./components/Layout";
+import Home from "./Pages/Home/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />} path="/">
-          <Route element={<MotherNature />} path="/" index/>
+          <Route element={<Home />} path="/" index/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import MotherNature from '../../components/MotherNature'
+
+const Home = () => {
+  return (
+    <>
+        {/* <Navbar/> */}
+        <MotherNature/>
+    </>
+  )
+}
+
+export default Home
